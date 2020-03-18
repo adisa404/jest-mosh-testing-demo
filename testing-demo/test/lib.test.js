1e0b550069dd1ac6eb2909250ad51442aab95bfe
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2020-03-18 16:07:14 +0100

    project - jest demo - test strings.

--- testing-demo/test/lib.test.js
+++ testing-demo/test/lib.test.js
@@ -27,3 +27,20 @@ describe('absolute', () => {
     expect(result).toBe(0);
   });
 });
+
+describe('greet', () => {
+  it('should return a greeting message', () => {
+    const message = lib.greet('Adisa');
+
+    expect(message).toBe('Welcome Adisa');
+  });
+
+  it('should return an almost exact greeting message', () => {
+    const message = lib.greet('Adisa');
+
+    expect(message).toContain('Adisa');
+    expect(message).toMatch(/Adisa/);
+  });
+});
+
+// test if a string is similar
